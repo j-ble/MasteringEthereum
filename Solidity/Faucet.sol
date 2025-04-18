0b92e93b8880fe39102334b2d3c9869c8c53fd80
Jacob Blemaster <ble@Jacobs-Mac-mini.local>
2025-04-18 09:48:41 -0700

    Bad faucet fix

--- Solidity/Faucet.sol
+++ Solidity/Faucet.sol
@@ -10,8 +10,8 @@ contract Faucet {
     function withdraw(uint withdraw_amount) public {
         // limit withdrawal amount
         require(withdraw_amount <= 100000000000000000);
-        
+
         // send the amount to the address that requested it
-        msg.sender.transfer(withdraw_amount);
+        payable(msg.sender).transfer(withdraw_amount);
     }
 }
