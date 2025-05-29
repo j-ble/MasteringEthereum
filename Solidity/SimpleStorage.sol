e284f121e76c7e799a852fd8c525d89b7ae4df45
j-ble <blemasterjacob@gmail.com>
2025-05-29 10:26:58 -0700

    Simple Storage

--- Solidity/SimpleStorage.sol
+++ Solidity/SimpleStorage.sol
@@ -3,14 +3,31 @@ pragma solidity ^0.8.19; // stating version
 
 contract SimpleStorage {
     // favorite number initalized to 0 if no value
-    uint256 public favoriteNumber; // defaults to 0
+    uint256 myFavoriteNumber; // defaults to 0
+
+    // uint256[] listOfFavoriteNumbers;
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // dynamic array
+    Person[] public listOfPeople;
+
+    // Person public pat = Person({favoriteNumber: 7, name: "Pat"});
+    // Person public john = Person({favoriteNumber: 16, name: "john"});
+    // Person public jacob = Person({favoriteNumber: 777, name: "jacob"});
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
 
     // view, pure
     function retrieve() public view returns(uint256){
-        return favoriteNumber;
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
     }
 }
