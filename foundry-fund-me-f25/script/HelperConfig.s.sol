aa34d8f3f4ec4154197764529a52936090ea6fed
Jacob <blemasterjacob@gmail.com>
2025-06-09 13:11:09 -0700

    Refactoring the SC pt.2

--- foundry-fund-me-f25/script/HelperConfig.s.sol
+++ foundry-fund-me-f25/script/HelperConfig.s.sol
@@ -11,13 +11,16 @@ contract HelperConfig is Script {
     // If we are on a local anvil chain, we can deploy the contract/mocks
     // Otherwise, grab the contract address from a live network
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
     NetworkConfig public activeNetworkConfig;
 
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateEthAnvilConfig();
         }
     }
 
@@ -33,13 +36,18 @@ contract HelperConfig is Script {
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public returns (NetworkConfig memory){
+    function getOrCreateEthAnvilConfig() public returns (NetworkConfig memory){
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
         // price feed address
         // 1. Deploy the mock
         // 2. Return the mock address
 
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE);
         vm.stopBroadcast();
 
         NetworkConfig memory anvilConfig = NetworkConfig({
