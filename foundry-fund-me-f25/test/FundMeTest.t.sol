7e4acba0515f58dafe19d230dd52223ec57c6f8c
Jacob <blemasterjacob@gmail.com>
2025-06-09 09:13:20 -0700

    Running tests on chain forks

--- foundry-fund-me-f25/test/FundMeTest.t.sol
+++ foundry-fund-me-f25/test/FundMeTest.t.sol
@@ -18,4 +18,25 @@ contract FundMeTest is Test{
     function testOwnerIsMsgSender() public view {
         assertEq(fundMe.i_owner(), address(this));
     }
+
+    // What can we do to work with addresses outside of our system?
+    // 1. Unit
+    //    - Testing a specific part of our code
+    // 2. Integration
+    //    - Testing the interaction between different parts of our code
+    // 3. Forked
+    //    - Testing our code on a simulated blockchain
+    // 4. Staging
+    //    - Testing our code on a real environment
+    
+    function testFundMeCanBeFunded() public {
+        vm.deal(address(this), 1 ether);
+        fundMe.fund{value: 1 ether}();
+        assertEq(fundMe.addressToAmountFunded(address(this)), 1 ether);
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
 }
