21b5c6194b0415ac9b917fdee0105c6b82497721
Jacob <blemasterjacob@gmail.com>
2025-06-09 10:09:43 -0700

    Refactoring mock smart contract

--- foundry-fund-me-f25/script/HelperConfig.s.sol
+++ foundry-fund-me-f25/script/HelperConfig.s.sol
@@ -5,9 +5,9 @@ pragma solidity ^0.8.24;
 // 2. Keep track of contract addresses across different chains
 
 import {Script} from "forge-std/Script.sol";
-import {FundMe} from "../src/FundMe.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
-contract HelperConfig {
+contract HelperConfig is Script {
     // If we are on a local anvil chain, we can deploy the contract/mocks
     // Otherwise, grab the contract address from a live network
 
@@ -33,11 +33,13 @@ contract HelperConfig {
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory){
+    function getAnvilEthConfig() public returns (NetworkConfig memory){
         // price feed address
-        NetworkConfig memory anvilConfig =  NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        });
-        return anvilConfig;
+        // 1. Deploy the mock
+        // 2. Return the mock address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
     }
 }
