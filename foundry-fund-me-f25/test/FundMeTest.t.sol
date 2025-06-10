e37c963eb00ed4dbe49417999e32a84c59f66971
Jacob <blemasterjacob@gmail.com>
2025-06-10 08:58:09 -0700

    Foundry tests cheatcodes

--- foundry-fund-me-f25/test/FundMeTest.t.sol
+++ foundry-fund-me-f25/test/FundMeTest.t.sol
@@ -7,15 +7,21 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test{
     FundMe fundMe;
+    uint256 constant SEND_VALUE = 0.1 ether; // 100000000000000000 wei
+    uint256 constant MINIMUM_USD = 5e18;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
+    address USER = makeAddr("user");
 
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFuneMe = new DeployFundMe();
         fundMe = deployFuneMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public view {
-        assertEq(fundMe.MINIMUM_USD(), 5e18);
+        assertEq(fundMe.MINIMUM_USD(), MINIMUM_USD);
     }
 
     function testOwnerIsMsgSender() public view {
@@ -32,14 +38,21 @@ contract FundMeTest is Test{
     // 4. Staging
     //    - Testing our code on a real environment
 
-    function testFundMeCanBeFunded() public {
-        vm.deal(address(this), 1 ether);
-        fundMe.fund{value: 1 ether}();
-        assertEq(fundMe.addressToAmountFunded(address(this)), 1 ether);
-    }
-
-    function testPriceFeedVersionIsAccurate() public view {
+    function testPriceFeedVersionIsAccurate() public view{
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert(); // Hey, we can revert here!
+        // assert, this Tx fails/reverts
+        fundMe.fund(); // send 0 value
+    }
+
+    function testFundUpdatesFundedDataStructures() public {
+        vm.prank(USER); // The next Tx will be sent by USER
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
