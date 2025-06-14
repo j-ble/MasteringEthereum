e5c37f9198d7033d6df2d0fbc0bde562cf7a13ba
j-ble <blemasterjacob@gmail.com>
2025-06-14 08:12:09 -0700

    Smart contract events

--- foundry-smart-contract-lottery-f25/src/Raffel.sol
+++ foundry-smart-contract-lottery-f25/src/Raffel.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
@@ -13,6 +34,10 @@ contract Raffel {
     error NotEnoughETH();
 
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffelEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
@@ -23,6 +48,9 @@ contract Raffel {
         if(msg.value < i_entranceFee){
             revert NotEnoughETH();
         }
+        s_players.push(payable(msg.sender));
+
+        emit RaffelEntered(msg.sender);
     }
 
     function pickWinner() public {
