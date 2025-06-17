ccced30f3dd9a58e972104b1a802fd0d04499c0e
j-ble <blemasterjacob@gmail.com>
2025-06-16 20:53:55 -0700

    lottery restart - resetting an array

--- foundry-smart-contract-lottery-f25/src/Raffel.sol
+++ foundry-smart-contract-lottery-f25/src/Raffel.sol
@@ -60,6 +60,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
 
     /* Events */
     event RaffelEntered(address indexed player);
+    event WinnerPicked(address indexed winner);
 
     constructor(
         uint256 entranceFee,
@@ -124,12 +125,17 @@ contract Raffle is VRFConsumerBaseV2Plus {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
+
         s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         if(!success) {
             revert Raffle__TransferFailed();
             // revert("Failed to send Ether");
         }
+        emit WinnerPicked(s_recentWinner);
     }
 
     /**
