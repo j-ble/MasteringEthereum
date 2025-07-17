d67bcde9dc376e6da1afed874bee6fc7be8f3e30
j-ble <blemasterjacob@gmail.com>
2025-07-17 14:11:08 -0700

    Create the fuzz tests handler pt.2

---
+++ foundry-defi-stablecoin-f25/test/fuzz/Handler.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "../../lib/forge-std/src/Test.sol";
+
+// Handler is going to narrow down the way we call functions
+
+contract Handler is Test {
+    
+}
---
+++ foundry-defi-stablecoin-f25/test/fuzz/Invariants.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "../../lib/forge-std/src/Test.sol";
+import {StdInvariant} from "../../lib/forge-std/src/StdInvariant.sol";
+import {DeployDSC} from "../../src/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../src/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Have our invariant aka properties that should always hold true
+
+// What are our invariants?
+// 1. The total supply of DSC should be less than total value of collateral
+// 2. Getter view functions should never revert <- evergreen invariant
+
+contract Invariants is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine engine;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    address wxrp;
+    address weth;
+    address wbtc;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, engine, config) = deployer.run();
+        (,,, weth, wbtc, wxrp,) = config.activeNetworkConfig();
+        targetContract(address(engine));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        //  Get the vlaue of all the collaterla in the protocol
+        // Compare it to  all the debt (dsc)
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(engine));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(engine));
+        uint256 totalWxrpDeposited = IERC20(wxrp).balanceOf(address(engine));
+
+        uint256 wethValue = engine.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = engine.getUsdValue(wbtc, totalWbtcDeposited);
+        uint256 wxrpValue = engine.getUsdValue(wxrp, totalWxrpDeposited);
+
+        console.log("weth Value: ", wethValue);
+        console.log("wbtc Value: ", wbtcValue);
+        console.log("wxrp Value: ", wxrpValue);
+        console.log("totalSupply: ", totalSupply);
+
+        assert ((wethValue + wbtcValue + wxrpValue) >= totalSupply);
+    }
+}
